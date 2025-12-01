41006bcac9fc8396e7f869825ed2426e051c8697
ilariamotta <ilaria.motta93@gmail.com>
2025-12-01 15:28:23 +0100

    feat: struttura post

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return "Hello World"
+  return (<>
+    <header className='flex justify-content-center'><h1>Your life in a blog</h1></header>
+    <main>
+      <div className="container flex justify-content-center">
+        <div className="blog-card">
+          <div className="card-title">
+            <h2>Lorem ipsum dolor sit amet!</h2>
+          </div>
+          <div className="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut maiores laboriosam, quibusdam debitis fugit fugiat ex repellat aspernatur? Quisquam quibusdam similique nulla perferendis beatae sit esse corrupti obcaecati autem eum.
+          Beatae saepe repellat, mollitia facere, aut culpa, molestias tempore deserunt cumque quam ea error quas cupiditate atque molestiae? Dolor, iure ad. Est quidem sed totam quia animi aperiam sint id!</div>
+        </div>
+        </div>
+        </main>
+  </>)
 }
 
 export default App
